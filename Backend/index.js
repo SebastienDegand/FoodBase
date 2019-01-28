98ff9315128939b4f904435de246ca4ad4338e69
Alexandre Clement <alexandre.clement@etu.unice.fr>
2019-01-28 16:01:47 +0100

    Setup backend app.

--- Backend/index.js
+++ Backend/index.js
@@ -1,1 +1,24 @@
-console.log('helloworld');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8080;
+
+const MongoClient = require('mongodb').MongoClient
+let db = null;
+
+MongoClient.connect('http://mongodb:admin:password06@ds111455.mlab.com:11455/foodbasedb')
+  .then(function (database) {
+    db = database;
+  })
+  .catch(function (err) {
+    console.log(err)
+  })
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/api/v1/foods', function() {
+
+})
+
+app.listen(port, () => {
+  console.log('listening on ' + port)
+})
