ea21029dd0e872f8e34bf5b2e40d5b51d920a8b2
Shiyang HUANG <shiyang.huang@etu.unice.fr>
2019-01-29 20:39:15 +0100

    get food by id, get all foods (return first 30 foods)

--- Backend/index.js
+++ Backend/index.js
@@ -1,24 +1,54 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 8080;
 
-const MongoClient = require('mongodb').MongoClient
+const MongoClient = require("mongodb").MongoClient;
+const url = "mongodb://admin:password06@ds111455.mlab.com:11455/foodbasedb";
 let db = null;
 
-MongoClient.connect('http://mongodb:admin:password06@ds111455.mlab.com:11455/foodbasedb')
-  .then(function (database) {
-    db = database;
-  })
-  .catch(function (err) {
-    console.log(err)
-  })
+app.use(express.static(__dirname + "/public"));
 
-app.use(express.static(__dirname + '/public'));
+app.get("/api/v1/foods", async function(req, res) {
+  try {
+    const client = await new MongoClient(url, { useNewUrlParser: true });
+    await client.connect();
+    const db = await client.db("foodbasedb");
+    let foods = await findFoods(db);
+    res.send(foods);
+  } catch (error) {
+    console.log(error);
+  }
+});
 
-app.get('/api/v1/foods', function() {
-
-})
+app.get("/api/v1/foods/:id", async function(req, res) {
+  try {
+    const client = await new MongoClient(url, { useNewUrlParser: true });
+    await client.connect();
+    const db = await client.db("foodbasedb");
+    let food = await findFoodsById(db, req.params.id);
+    res.send(food);
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 app.listen(port, () => {
-  console.log('listening on ' + port)
-})
+  console.log("listening on " + port);
+});
+
+async function findFoods(db) {
+  const collection = await db.collection("france");
+  const foods = await collection
+    .find({})
+    .limit(30)
+    .toArray();
+  console.log("foods found");
+  return foods;
+}
+
+async function findFoodsById(db, id) {
+  const collection = await db.collection("france");
+  const food = await collection.findOne({ _id: id });
+  console.log("foods " + id + " found");
+  return food;
+}
