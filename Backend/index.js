3290547ed79da590e1d2bd314e78b529aee12b48
Shiyang HUANG <shiyang.huang@etu.unice.fr>
2019-01-31 11:39:09 +0100

    implement insert pricing

--- Backend/index.js
+++ Backend/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 8080;
+const bodyParser = require("body-parser");
 
 const MongoClient = require("mongodb").MongoClient;
 const url = "mongodb://admin:password06@ds111455.mlab.com:11455/foodbasedb";
 let db = null;
 
+app.use(bodyParser.json());
 app.use(express.static(__dirname + "/public"));
 
 app.get("/api/v1/foods", async function(req, res) {
@@ -49,6 +51,25 @@ app.get("/api/v1/foods/:id", async function(req, res) {
   }
 });
 
+app.put("/api/v1/foods/:id", async function(req, res) {
+  try {
+    let price = req.body.price;
+    let store = req.body.store;
+    let entryDate = +new Date();
+    let pricing = {};
+    pricing.price = price;
+    pricing.store = store;
+    pricing.entryDate = entryDate;
+    const client = await new MongoClient(url, { useNewUrlParser: true });
+    await client.connect();
+    const db = await client.db("foodbasedb");
+    let food = await updateFood(db, req.params.id, pricing);
+    res.send(food);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
 app.listen(port, () => {
   console.log("listening on " + port);
 });
@@ -67,6 +88,25 @@ async function findFoods(db, pagination) {
 async function findFoodsById(db, id) {
   const collection = await db.collection("france");
   const food = await collection.findOne({ _id: id });
-  console.log("foods " + id + " found");
+  console.log("food " + id + " found");
+  return food;
+}
+
+async function updateFood(db, id, pricing) {
+  const collection = await db.collection("france");
+  await collection.updateOne(
+    { _id: id },
+    {
+      $set: {
+        pricing: {
+          price: pricing.price,
+          store: pricing.store,
+          entryDate_t: pricing.entryDate
+        }
+      }
+    }
+  );
+  const food = await collection.findOne({ _id: id });
+  console.log("food " + id + " updated");
   return food;
 }
