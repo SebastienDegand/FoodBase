675963e5eab4abe478e8db19393bf4f8fd3c57b6
dq610272 <quentin.duret@etu.unice.fr>
2019-01-31 11:35:39 +0100

    navigation bar

---
+++ Frontend/src/components/Navigation.vue
@@ -0,0 +1,55 @@
+<template>
+<div>
+  <v-toolbar>
+    <v-toolbar-title>FoodBase</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-toolbar-items class="hidden-sm-and-down">
+      <v-btn @click="productClick()" flat>Produits</v-btn>
+      <v-btn @click="recipClick()" flat>Recette</v-btn>
+      <v-btn @click="mapClick()" flat>Carte</v-btn>
+    </v-toolbar-items>
+  </v-toolbar>
+  <h1 v-if="this.isProduct">Produits</h1>
+  <h1 v-if="this.isRecip">Recette</h1>
+  <h1 v-if="this.isMap">Carte</h1>
+</div>
+</template>
+
+<script>
+
+export default {
+    
+
+	data() {
+      
+		return {
+			isProduct : true,
+            isRecip : false,
+            isMap : false
+		}
+	},methods: {
+        productClick: function () {
+            this.isProduct = true
+            this.isRecip = false
+            this.isMap = false
+        },
+        recipClick: function () {
+            this.isProduct = false
+            this.isRecip = true
+            this.isMap = false
+        },
+        mapClick: function () {
+            this.isProduct = false
+            
+            this.isRecip = false
+            this.isMap = true
+        }
+    },
+	components: { 				
+		
+	}
+}
+</script>
+
+<style scoped>
+</style>
--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import Vuetify from 'vuetify'
+import Navigation from './components/Navigation'
 import 'vuetify/dist/vuetify.min.css'
 
 Vue.use(Vuetify)
+
+Vue.component("navigation", Navigation);
 
 new Vue({
   el: '#app',
