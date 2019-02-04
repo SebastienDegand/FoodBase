8a45f22441c0de09e3f580a1175353523541164f
sebastien <sebastien.degand@etu.unice.fr>
2019-02-04 14:14:33 +0100

    add allow cors

--- Backend/index.js
+++ Backend/index.js
@@ -9,6 +9,12 @@ let db = null;
 
 app.use(bodyParser.json());
 app.use(express.static(__dirname + "/public"));
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Methods", "*");
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.get("/api/v1/foods", async function(req, res) {
   try {
