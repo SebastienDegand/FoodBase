ef447d4843f3e00c37476cb347cfd58f102ff8fc
sebastien <sebastien.degand@etu.unice.fr>
2019-02-09 22:30:18 +0100

    add filter frontend

--- Frontend/src/ProductTab.vue
+++ Frontend/src/ProductTab.vue
@@ -1,8 +1,157 @@
 <template>
+  <div>
   <v-card>
     <v-card-title>
-      Food
+
+
+
+      <v-autocomplete
+        v-model="modelAlergenes"
+        :items="alergenes"
+        box
+        chips
+        color="blue-grey lighten-2"
+        label="Search without alergenes"
+        item-text="name"
+        item-value="name"
+        multiple
+        :disabled="radioModelAlergenes"
+      >
+        <template
+          slot="selection"
+          slot-scope="data"
+        >
+          <v-chip
+            :selected="data.selected"
+            close
+            class="chip--select-multi"
+            @input="removeAlergene(data.item)"
+          >
+            {{ data.item }}
+          </v-chip>
+        </template>
+        <template
+          slot="item"
+          slot-scope="data"
+        >
+          <template>
+            <v-list-tile-content v-text="data.item"></v-list-tile-content>
+          </template>
+        </template>
+      </v-autocomplete>
+
+      <v-radio-group v-model="radioModelAlergenes">
+        <v-radio
+          :key="0"
+          :label="`some`"
+          :value="false"
+        ></v-radio>
+        <v-radio
+          :key="1"
+          :label="`any`"
+          :value="true"
+        ></v-radio>
+      </v-radio-group>
+
       <v-spacer></v-spacer>
+
+      <v-autocomplete
+        v-model="modelShop"
+        :items="shops"
+        box
+        chips
+        color="blue-grey lighten-2"
+        label="Select shops"
+        item-text="name"
+        item-value="name"
+        multiple
+      >
+        <template
+          slot="selection"
+          slot-scope="data"
+        >
+          <v-chip
+            :selected="data.selected"
+            close
+            class="chip--select-multi"
+            @input="removeShop(data.item)"
+          >
+            {{ data.item }}
+          </v-chip>
+        </template>
+        <template
+          slot="item"
+          slot-scope="data"
+        >
+          <template>
+            <v-list-tile-content v-text="data.item"></v-list-tile-content>
+          </template>
+        </template>
+      </v-autocomplete>
+
+      <v-spacer></v-spacer>
+
+      <v-autocomplete
+        v-model="modelAdditives"
+        :items="additives"
+        box
+        chips
+        color="blue-grey lighten-2"
+        label="Search without additives"
+        item-text="name"
+        item-value="name"
+        multiple
+        :disabled="radioModelAdditives"
+      >
+        <template
+          slot="selection"
+          slot-scope="data"
+        >
+          <v-chip
+            :selected="data.selected"
+            close
+            class="chip--select-multi"
+            @input="removeAdditive(data.item)"
+          >
+            {{ data.item }}
+          </v-chip>
+        </template>
+        <template
+          slot="item"
+          slot-scope="data"
+        >
+          <template>
+            <v-list-tile-content v-text="data.item"></v-list-tile-content>
+          </template>
+        </template>
+      </v-autocomplete>
+
+      <v-radio-group v-model="radioModelAdditives">
+        <v-radio
+          :key="0"
+          :label="`some`"
+          :value="false"
+        ></v-radio>
+        <v-radio
+          :key="1"
+          :label="`any`"
+          :value="true"
+        ></v-radio>
+      </v-radio-group>
+
+    </v-card-title>
+
+    <v-card-title>
+      <v-flex xs12 sm6 d-flex>
+        <v-select
+          v-model="filterModel"
+          :items="filters"
+          label="Filter By"
+          outline
+        ></v-select>
+        <v-spacer></v-spacer>
+        <v-btn color="blue" dark large>Search</v-btn>
+      </v-flex>
     </v-card-title>
     <v-data-table
       :headers="headers"
@@ -16,19 +165,8 @@
       </template>
 
     </v-data-table>
-    <!-- <div class="text-xs-center pt-2">
-      <v-pagination v-model="pagination.currentPage"
-                    :length="pagination.totalPages"
-                    @input="getProductsToShow">
-      </v-pagination>
-      <v-flex xs1 sm1 d-flex>
-        <v-select v-model="paginationSync.rowsPerPage"
-          :items="pagination.possibleRowPerPage"
-          label="per page"
-        ></v-select>
-      </v-flex>
-    </div> -->
   </v-card>
+  </div>
 </template>
 
 <script>
@@ -36,6 +174,7 @@
         name: "ProductTab",
         data () {
           return {
+            products: [],
             headers: [
               {
                 text: 'Product name',
@@ -44,10 +183,26 @@
                 value: 'product_name'
               }
             ],
-            products: [],
             currentPage: 1,
             rowsPerPage: 60,
-            loadingProduct: false
+            loadingProduct: false,
+            isLoading: false,
+
+
+            alergenes: ["milk", "egg", "egg", "a", "b", "c", "d", "e"],
+            modelAlergenes: null,
+            radioModelAlergenes: false,
+
+            shops: ["carrefour", "auchan"],
+            modelShop: null,
+            radioModelShops: false,
+
+            additives: ["E1XX", "E2XX"],
+            modelAdditives: null,
+            radioModelAdditives: false,
+
+            filters: ["price", "nutrition value"],
+            filterModel: null
           }
         },
       mounted () {
@@ -76,6 +231,7 @@
             //this.products = data.filter(product => product.product_name);
             this.products = this.products.concat(data);
             this.loadingProduct = false;
+            console.log(this.products)
           })
         },
           showAlert: function(item) {
@@ -89,6 +245,22 @@
               this.getNextProducts()
             }
           }
+        },
+        removeAlergene (item) {
+          const index = this.modelAlergenes.indexOf(item);
+          if (index >= 0) this.modelAlergenes.splice(index, 1)
+        },
+        removeShop (item) {
+          const index = this.modelShop.indexOf(item);
+          if (index >= 0) this.modelShop.splice(index, 1)
+        },
+        removeAdditive (item) {
+          const index = this.modelAdditives.indexOf(item);
+          if (index >= 0) this.modelAdditives.splice(index, 1)
+        },
+
+        computeCriteria() {
+
         }
       },
       computed: {
