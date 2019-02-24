57dfd9da4cfb33b0c708832ae9dee877dc7c8ab2
sebastien <sebastien.degand@etu.unice.fr>
2019-02-24 17:44:58 +0100

    add production variable

--- Frontend/webpack.config.js
+++ Frontend/webpack.config.js
@@ -83,7 +83,7 @@ if (process.env.NODE_ENV === 'development') {
   module.exports.plugins = (module.exports.plugins || []).concat([
     new webpack.DefinePlugin({
       'process.env': {
-        NODE_ENV: '"production"',
+        NODE_ENV: '"development"',
         BACKEND_API: '"http://localhost:8080/api/v1"'
       }
     }),
