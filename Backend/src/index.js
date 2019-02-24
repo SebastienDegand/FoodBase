cbcc56897ce4d45a44a3dd2e6f9f0522e842a43c
sebastien <sebastien.degand@etu.unice.fr>
2019-02-24 14:03:05 +0100

    update cros origin rule to allow PUT

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -12,7 +12,7 @@ let db = null;
 app.use(bodyParser.json());
 app.use(express.static(__dirname + "/public"));
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Methods", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
